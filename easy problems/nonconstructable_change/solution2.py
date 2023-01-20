511b3ec994660a022f143d6532e818df7d0f03fe
eldorfozilov <eldorfozilov@unist.ac.kr>
2023-01-20 20:13:15 +0900

    optimal solution

---
+++ easy problems/nonconstructable_change/solution2.py
@@ -0,0 +1,12 @@
+# Time Complexity: O(nlog(n))
+# Space Complexity: O(1)
+
+def nonConstructibleChange(coins):
+    coins.sort()
+    current_change = 0
+    for coin in coins:
+        if coin > current_change + 1:
+            return current_change + 1
+        current_change += coin
+    
+    return current_change + 1
