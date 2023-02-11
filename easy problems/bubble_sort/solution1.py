bba017904c44acae2e46a0783891c4eba6ff99c8
eldorfozilov <eldorfozilov@unist.ac.kr>
2023-02-11 14:55:01 +0900

    bubble sort

---
+++ easy problems/bubble_sort/solution1.py
@@ -0,0 +1,20 @@
+# Time Complexity: O(n), n is the number of elements in the array
+# Auxiliary Space Complexity: O(1)
+
+
+def bubbleSort(array):
+
+    while True:
+        swap_num = 0
+        count_iter = 1
+        for i in range(len(array) - count_iter):
+            prev = array[i]
+            next = array[i+1]
+            if prev > next:
+                array[i] = next
+                array[i+1] = prev
+                swap_num += 1
+
+        if swap_num == 0:
+            return array
+        count_iter += 1
---
+++ easy problems/bubble_sort/solution2.py
@@ -0,0 +1,17 @@
+# Time Complexity: O(n), n is the number of elements in the array
+# Auxiliary Space Complexity: O(1)
+
+def bubbleSort(array):
+
+    isSorted = False
+    while not isSorted:
+        isSorted = True
+        for i in range(len(array) - 1):
+            if array[i] > array[i+1]:
+                swap(i,i+1, array)
+                isSorted = False
+    
+    return array
+
+def swap(i,j, array):
+    array[i], array[j] = array[j], array[i]
